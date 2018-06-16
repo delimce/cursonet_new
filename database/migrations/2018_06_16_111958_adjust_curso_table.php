abfa4da7eea932ff14d8933fdb9e19009bc32a9c
Luis De Lima <delimce@gmail.com>
2018-06-16 17:29:06 -0400

    config migrations and set relationship resp for course

---
+++ database/migrations/2018_06_16_111958_adjust_curso_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AdjustCursoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        //
+        Schema::table('tbl_curso', function (Blueprint $table) {
+
+
+            $table->integer('resp')->default(null)->change();
+            $table->integer('resp', 11)->change();
+            $table->unsignedInteger('resp')->change();
+
+            $table->foreign('resp')
+                ->references('id')->on('tbl_admin')
+                ->onDelete('set null');
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+        Schema::table('tbl_curso', function (Blueprint $table) {
+
+            $table->integer('resp')->unsigned()->change();
+            $table->dropForeign('tbl_curso_resp_foreign');
+
+        });
+    }
+}
