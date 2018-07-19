cdd9d0a82959a02b5085c2ab52f057038abb4f5f
Luis De Lima <delimce@gmail.com>
2018-07-19 09:22:40 -0400

    if there are no topics, show prompt

--- resources/views/student/pages/classroom/modules.blade.php
+++ resources/views/student/pages/classroom/modules.blade.php
@@ -1,10 +1,14 @@
 <section>
     <h2>@lang('students.classroom.modules.select')</h2>
-    @foreach ($topics as $i => $module)
-        <div class="module-item @if ($i==0) current-module @endif" data-topic="{{$module->id}}">
-            <span>{{$i+1}}.&nbsp;{{$module->titulo}}</span>
-        </div>
-    @endforeach
+    @if($topics->count()==0)
+        <span>@lang('students.classroom.modules.nofound')</span>
+    @else
+        @foreach ($topics as $i => $module)
+            <div class="module-item @if ($i==0) current-module @endif" data-topic="{{$module->id}}">
+                <span>{{$i+1}}.&nbsp;{{$module->titulo}}</span>
+            </div>
+        @endforeach
+    @endif
 </section>
 
 @push('scripts')
@@ -13,7 +17,6 @@
             var topic_id = $(this).data('topic');
             $(".current-module").removeClass("current-module")
             $(this).addClass("current-module");
-
         })
     </script>
 @endpush
