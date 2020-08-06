739e76c51f5dbb4c0f326c9a0094e05366f2fdf9
delimce <delimce@gmail.com>
2020-08-06 11:07:28 +0200

    move to center logo and texts in email layout

--- resources/views/student/layout/email.blade.php
+++ resources/views/student/layout/email.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="es">
-<body style="background-image: url('{!! url("assets/img/email_back.png")  !!}');  -webkit-background-size: cover; ">
+<body 
+style="background-image: url('{!! url("assets/img/email_back.png")  !!}');
+    text-align:center;
+  -webkit-background-size: cover; ">
 <div style="padding: 14px ">
     <img src="{!! url("assets/img/logo_student.png") !!}">
 </div>
