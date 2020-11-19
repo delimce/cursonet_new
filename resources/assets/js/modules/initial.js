136b2ba313cf4af236e20d8dd464525560961bad
delimce <delimce@gmail.com>
2020-11-20 00:57:13 +0100

    login with throttle method

--- resources/assets/js/modules/initial.js
+++ resources/assets/js/modules/initial.js
@@ -1,17 +1,23 @@
-$("#login_form").submit(function (event) {
+
+$("#login_form").on("submit", _.throttle(function (e) {
+    doLogin(e);
+}, 1000))
+
+
+const doLogin = function (event) {
     axios.post(api_url + 'student/doLogin', {
         email: $("#email").val(),
         password: $("#password").val()
     }).then(function (response) {
-        console.log(response);
         redirect(api_url + 'student/home', false)
     }).catch(function (error) {
         showAlert(error.response.data.message)
     });
     event.preventDefault();
-});
+}
 
-$("#forgot_form").submit(function (event) {
+
+$("#forgot_form").on("submit", function (event) {
     $("#bforget").html('Enviando').prop('disabled', true);
     axios.post(api_url + 'api/student/forgotten', {
         email: $("#email").val()
@@ -23,7 +29,7 @@ $("#forgot_form").submit(function (event) {
         showAlert(error.response.data.message)
     });
     event.preventDefault();
-});
+})
 
 $('#register_form').bootstrapValidator().on('success.form.bv', function (e) {
     // Prevent form submission
@@ -33,12 +39,12 @@ $('#register_form').bootstrapValidator().on('success.form.bv', function (e) {
     let $form = $(e.target);
     axios.post(api_url + 'api/student/signup', $form.serialize())
         .then(function (response) {
-            redirect(api_url +"student/register/success/" + $("#email").val(), false)
+            redirect(api_url + "student/register/success/" + $("#email").val(), false)
         }).catch(function (error) {
-        console.log(error);
-        grecaptcha.reset();
-        showAlert(error.response.data.message);
-        $("#bregister").html('Registrar').prop('disabled', false);
-    });
+            console.log(error);
+            grecaptcha.reset();
+            showAlert(error.response.data.message);
+            $("#bregister").html('Registrar').prop('disabled', false);
+        });
 
 })
